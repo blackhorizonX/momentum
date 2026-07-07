081f231727e5233a1d0a540c96c0626fc323b4bf
walt west <waltwest@outlook.com>
2026-07-07 18:34:38 -0400

    refining habit creation/saving functions

--- src/habit.c
+++ src/habit.c
@@ -35,30 +35,36 @@ Habit* init_habit(char name[]) {
 // save habit
 int save_habit(Habit* h) {
     // create file path
-    char file[STR_LENGTH] = "";
-    char path[STR_LENGTH] = "data/\0";
-    
-    strcat(file, h->h_name); // add file name
-    strcat(file, ".txt"); // file extension
-    strcat(path, file); // add file onto dir
+    char file[STR_LENGTH];
+
+    snprintf(path, sizeof(path), "data/%s.txt", h->h_name);
+
     printf("Full path: %s\n", path);
 
-    // create file
-    if (fopen(path, "r") != NULL) { // file already exists
+    // check if file already exists
+    FILE *check = fopen(path, "r");
+    if (check != NULL) {
+        fclose(check);
         printf("File \"%s\" already exists.\n", path);
+
         return 1;
-
-    } else if (fopen(path, "r") == NULL) { //create file
-        FILE *fptr = fopen(path, "w");
-
-        if (fptr == NULL) { // file creation failed
-            printf("File creation for \"%s\" failed\n", path);
-            return 1;
-        } else { // file creation success
-            printf("Habit data created at \"%s\"\n", path);
-            fclose(fptr);
-        }
     }
+
+    FILE *fptr = fopen(path, "w");
+    if (fptr == NULL) { // file creation failed
+        printf("File creation for \"%s\" failed\n", path);
+
+        return 1;
+        }  
+
+    fprintf(fptr, "h_name:%s\n", h->h_name);
+    fprintf(fptr, "init_date:%s\n", h->init_date);
+    fprintf(fptr, "reset:false\n");
+    fprintf(fptr, "reset_date:\n");
+    fprintf(fptr, "best:0\n", h->h_name);
+    printf("Habit data created at \"%s\"\n", path);
+    fclose(fptr);
 
     return 0;
 };
+
--- src/menu.c
+++ src/menu.c
@@ -24,36 +24,6 @@ int get_option(int x) {
 }
 
 
-// save habit
-int save_habit(Habit* h) {
-    // create file path
-    char file[STR_LENGTH] = "";
-    char path[STR_LENGTH] = "data/\0";
-    
-    strcat(file, h->h_name); // add file name
-    strcat(file, ".txt"); // file extension
-    strcat(path, file); // add file onto dir
-    printf("Full path: %s\n", path);
-
-    // create file
-    if (fopen(path, "r") != NULL) { // file already exists
-        printf("File \"%s\" already exists.\n", path);
-        return 1;
-
-    } else if (fopen(path, "r") == NULL) { //create file
-        FILE *fptr = fopen(path, "w");
-
-        if (fptr == NULL) { // file creation failed
-            printf("File creation for \"%s\" failed\n", path);
-            return 1;
-        } else { // file creation success
-            printf("Habit data created at \"%s\"\n", path);
-            fclose(fptr);
-        }
-    }
-
-    return 0;
-};
 
 // display stats from active habits
 int dashboard() {
